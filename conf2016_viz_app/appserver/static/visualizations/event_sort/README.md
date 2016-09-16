aa8349f2f980f98ec21b8ac71ac25a3902e285e1
Marshall Agnew <magnew@splunk.com>
2016-09-16 15:38:32 -0700

    D34 experiments

--- conf2016_viz_app/appserver/static/visualizations/event_sort/README.md
+++ conf2016_viz_app/appserver/static/visualizations/event_sort/README.md
@@ -2,6 +2,6 @@
 
 ## Sample Searches
 ```
-index=_internal sourcetype=splunkd  
+index=_internal sourcetype=splunk_web* | head 100000 | table _time sourcetype
 
 ```
--- conf2016_viz_app/appserver/static/visualizations/event_sort/src/event_sort.js
+++ conf2016_viz_app/appserver/static/visualizations/event_sort/src/event_sort.js
@@ -30,109 +30,126 @@ define([
             this.initialRun = true;
         },
 
-        setupView: function() {
-            // Here we set up the initial view layout
-            var margin = {top: 30, right: 30, bottom: 30, left: 30};
-            var availableWidth = parseInt(this.$el.width(), 10);
-            var availableHeight = parseInt(this.$el.height(), 10);
-
-            var svg = d3.select(this.el)
-                .append('svg')
-                .attr('width', availableWidth)
-                .attr('height', availableHeight)
-                .attr('pointer-events', 'all');
-
-            this._viz = { container: this.$el, svg: svg, margin: margin};
-        },
-
         formatData: function(data) {
-            console.log('Format data with', data);
+            console.log('Format data ', data);
 
             if (data.rows.length < 1 || data.fields.length < 1) {
                 return false;
             }
-    
-            // Get unique values of the second column
-            var buckets = _(data.rows).chain().unzip().rest().first().uniq().value();
             
-            return {
-                rows: data.rows,
-                buckets: buckets
-            };
+            if(data.fields[0].name === '_time'){
+                
+                // Get unique values of the second column
+                var buckets = _(data.rows).chain().unzip().rest().first().uniq().value();
+                
+                return {
+                    rows: data.rows,
+                    buckets: buckets
+                };
+            }
+            else {
+                return _.map(data.rows, function(row){
+                    return {
+                        name: row[0],
+                        value: row[1]
+                    }
+                });
+            }
         },
  
         updateView: function(data, config) {
 
-            if (!data.rows || data.rows.length < 1) {
-                return
+            console.log('Update', data);
+
+            if(!data){
+                return;
             }
+
+            this.$el.empty();
             
-            var containerHeight = this.$el.height();
-            var containerWidth = this.$el.width(); 
+            var margin = {top: 20, right: 20, bottom: 70, left: 40},
+                width = 600 - margin.left - margin.right,
+                height = 300 - margin.top - margin.bottom;
 
-            var svg = $(this._viz.svg[0]);
-            svg.height(containerHeight);
-            svg.width(containerWidth);
-
-            if(!_.isEqual(data.buckets, this.buckets)){
-                this.buckets = data.buckets
-                this._drawConstantObjects(containerWidth, containerHeight);
-            }
-
-            return;
-
-            console.log('dataRows', dataRows);
-            if (!dataRows || dataRows.length === 0 || dataRows[0].length === 0) {
-                return this;
-            }
-            var fields = data.fields;
-
-            console.log('fields', fields);
-
-            this.oldTimes = this.times || [];
-            this.times = _.map(dataRows, function(row){
-                return row[0];
+            var names = _.map(data, function(row){ 
+                return row.name; 
             });
 
-            console.log('this.times', this.times);
-            var newTimes = _.difference(this.times, this.oldTimes);
+            var maxValue = _.max(_.map(data, function(row){ 
+                return row.value
+            }));
 
-            var newNodes = _.filter(dataRows, function(row){
-                return _.contains(newTimes, row[0]);
-            });
+            var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05).domain(names);
+            var y = d3.scaleLinear().range([height, 0]).domain([0, maxValue]);
 
-            if(!this.initialRun){
-                this.nodeQueue = this.nodeQueue.concat(newNodes);
-            }
+            var xAxis = d3.axisBottom(x);
 
-            this.initialRun = false;
+            var yAxis = d3.axisLeft(y)
+                .ticks(10);
+
+            var svg = d3.select(this.el).append('svg')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+                .attr('transform', 
+                    'translate(' + margin.left + ',' + margin.top + ')');
+
+            // x.domain(data.map(function(d) { return d.date; }));
+            //y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+            svg.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', 'translate(0,' + height + ')')
+                .call(xAxis)
+                .selectAll('text')
+                .style('text-anchor', 'end')
+                .attr('dx', '-.8em')
+                .attr('dy', '-.55em')
+                .attr('transform', 'rotate(-90)' );
+
+            svg.append('g')
+                .attr('class', 'y axis')
+                .call(yAxis)
+                .append('text')
+                .attr('transform', 'rotate(-90)')
+                .attr('y', 6)
+                .attr('dy', '.71em')
+                .style('text-anchor', 'end')
+                .text('Value ($)');
+
+            var bars = svg.selectAll('bar')
+                .data(data).enter().append('rect')
+                .style('fill', 'steelblue')
+                .attr('x', function(d) { return x(d.name); })
+                .attr('width', x.bandwidth())
+                .attr('y', function(d) { return y(d.value); })
+                .attr('height', function(d) { return height - y(d.value); });
 
             return this;
         },
 
         _drawConstantObjects: function(){
 
-            var $svg = $(this._viz.svg[0]);
+            var $svg = $(this._viz.svg);
             $svg.empty();
             
             var graphWidth = $svg.width()
             var graphHeight = $svg.height();
             var graph = this._viz.svg;
 
-            var color = d3.scale.category20b();
+            var color = d3.scaleOrdinal(d3.schemeCategory10)
 
-            var xScale = d3.scale.ordinal()
-                    .domain(this.buckets)
-                    .rangeBands([this._viz.margin.left , graphWidth - this._viz.margin.right]);
+            debugger;
 
-            var xAxis = d3.svg.axis()
-                .scale(xScale)
-                .orient("bottom")
-                .tickSize(0);
+            var xScale = d3.scaleBand().rangeRound([this._viz.margin.left , graphWidth - this._viz.margin.right]).paddingInner(0.05)
+                    .domain(this.buckets);
+                    
 
-            var axisContainer = graph.append('g')
+            var xAxis = d3.axisBottom(xScale);
+
+            graph.append('g')
                 .attr('class', 'xaxis axis')
-                .attr("transform", "translate(0," + (graphHeight - 30) + ")")
+                .attr('transform', 'translate(0,' + (graphHeight - 30) + ')')
                 .call(xAxis);
             
             var sources = graph.selectAll('circle')
@@ -189,7 +206,7 @@ define([
 
             var nodes = force.nodes();
 
-            force.on("tick", function(e) {
+            force.on('tick', function(e) {
                 var k = e.alpha * .07;
                 nodes.forEach(function(node) {
                     
@@ -198,9 +215,9 @@ define([
                     node.y += (center.y - node.y) * k;
                 });
 
-                svg.selectAll("circle")
-                    .attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });                
+                svg.selectAll('circle')
+                    .attr('cx', function(d) { return d.x; })
+                    .attr('cy', function(d) { return d.y; });                
             });
 
             function fill(d){
